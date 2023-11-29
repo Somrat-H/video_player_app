15b08fe1f5cb63a633b9b972edf50d4c58ac465a
Somrat Hossain <somrat945hossain@gmail.com>
2023-11-29 23:34:03 +0600

    custom http and custom provider create

---
+++ lib/http/custom_http.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:video_player_app/model/video%20_model.dart';
+
+class CustomHttp {
+  static Future<VideoModel> getVideo(int page) async {
+    VideoModel? videoModel;
+
+    try {
+      var response = await http.get(Uri.parse("https://test-ximit.mahfil.net/api/trending-video/1?format=api&page=$page"));
+      var getData = jsonDecode(response.body);
+      videoModel = VideoModel.fromJson(getData);
+      return videoModel;
+
+    } catch (e) {
+      debugPrint(e.toString());
+      return videoModel!;
+    }
+  }
+}
---
+++ lib/provider/custom_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:video_player_app/http/custom_http.dart';
+import 'package:video_player_app/model/video%20_model.dart';
+
+class CustomProvider with ChangeNotifier{
+  VideoModel ?videoModel;
+
+  Future<VideoModel> getVide(int page)async{
+    videoModel = await CustomHttp.getVideo(page);
+    return videoModel!;
+  }
+
+}
