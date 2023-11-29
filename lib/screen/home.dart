a8c3c894e1a43902bd3b47f39a486e6431bef4cd
Somrat Hossain <somrat945hossain@gmail.com>
2023-11-30 02:03:01 +0600

    create custom video list card

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:video_player_app/model/video%20_model.dart';
 import 'package:video_player_app/provider/custom_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:video_player_app/screen/widget/custom_video_card.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -23,10 +24,12 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
             Text("Trending Video", style: TextStyle(
               fontSize: 18,
@@ -37,23 +40,22 @@ class _HomeState extends State<Home> {
             
             builder: (context, snapshot){
               if(snapshot.hasData){
-                   return SizedBox(
-              width: double.infinity,
-              child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: NetworkImage(snapshot.data!.results![1].thumbnail.toString(), scale: 1.0, ),
+                   return Expanded(
+                     child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.results!.length,
+                      itemBuilder: (_, index)=> 
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: CustomVideoCard(
+                        channelImage: snapshot.data!.results![index].channelImage.toString(), 
+                        title: snapshot.data!.results![index].title.toString(), 
+                        date: snapshot.data!.results![index].dateAndTime.toString(), 
+                        image: snapshot.data!.results![index].thumbnail.toString(), 
+                        views: snapshot.data!.results![index].viewers.toString()),
                       )
-                    ),
-                  )
-                ],
-              ),
-            );
+                     ),
+                   );
               }
               else {
                 return Text("No data");
