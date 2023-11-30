03c818b1c1574c82cc07df18ff04e52e98df56de
Somrat Hossain <somrat945hossain@gmail.com>
2023-11-30 14:04:01 +0600

    for infinite scroling

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:video_player_app/model/video%20_model.dart';
 import 'package:video_player_app/provider/custom_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:video_player_app/screen/video_play.dart';
 import 'package:video_player_app/screen/widget/custom_video_card.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,16 +15,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  ScrollController scrollController = ScrollController();
+ 
+  int _page = 1;
+  int _offset = 2;
+  bool isLoad = false;
+
+  void update(){
+
+  }
  
   @override
   void initState() {
-   
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+
+            isLoad = true;
+            setState(() {
+              
+              _offset = _offset + _page;
+              CustomProvider().getOffSetData(_page,_offset);
+            });
+            isLoad = false;
+        
+      }
+    });
     // TODO: implement initState
     super.initState();
   }
   
   @override
   Widget build(BuildContext context) {
+     final cart = Provider.of<CustomProvider>(context);
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: SafeArea(
@@ -35,35 +60,54 @@ class _HomeState extends State<Home> {
               fontSize: 18,
               fontWeight: FontWeight.bold,
             ),),
-            FutureBuilder(
-              future: CustomProvider().getVide(1), 
+            FutureBuilder<List<Results>>(
+              future: cart.getVide(_page, _offset), 
             
             builder: (context, snapshot){
               if(snapshot.hasData){
                    return Expanded(
                      child: ListView.builder(
+                      controller: scrollController,
                       shrinkWrap: true,
-                      itemCount: snapshot.data!.results!.length,
+                      itemCount: snapshot.data!.length,
                       itemBuilder: (_, index)=> 
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: CustomVideoCard(
-                        channelImage: snapshot.data!.results![index].channelImage.toString(), 
-                        title: snapshot.data!.results![index].title.toString(), 
-                        date: snapshot.data!.results![index].dateAndTime.toString(), 
-                        image: snapshot.data!.results![index].thumbnail.toString(), 
-                        views: snapshot.data!.results![index].viewers.toString()),
+                          onTap: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (_)=> VideoPlay(
+                              thumbnail: snapshot.data![index].thumbnail.toString(),
+                              channelImage: snapshot.data![index].channelImage.toString(),
+                              channelName: snapshot.data![index].channelName.toString(), 
+                              days: snapshot.data![index].dateAndTime.toString(), 
+                              channelSubscriber: snapshot.data![index].channelSubscriber.toString(), 
+                              title: snapshot.data![index].title.toString(), 
+                              views: snapshot.data![index].viewers.toString(), 
+                              manifest: snapshot.data![index].manifest.toString())));
+                          },
+                        channelImage: snapshot.data![index].channelImage.toString(), 
+                        title: snapshot.data![index].title.toString(), 
+                        date: snapshot.data![index].dateAndTime, 
+                        image: snapshot.data![index].thumbnail.toString(), 
+                        views: snapshot.data![index].viewers.toString()) 
                       )
                      ),
                    );
               }
               else {
-                return Text("No data");
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.grey,
+                  ),
+                );
               }
              
             }
             
-            )
+            ),
+             isLoad == true?Center(
+                    child: CircularProgressIndicator(),
+                  ): Text(""),
             
                     ],
                   ),
