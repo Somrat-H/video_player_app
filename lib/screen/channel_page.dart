5895919d5a614adfda520535e61346a5e2a9c27b
Somrat Hossain <somrat945hossain@gmail.com>
2023-11-30 14:27:05 +0600

    eempty page for channel page created

---
+++ lib/screen/channel_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(onPressed: (){
+          Navigator.pop(context);
+        }, icon: Icon(Icons.arrow_back)),
+      ),
+    );
+  }
+}
